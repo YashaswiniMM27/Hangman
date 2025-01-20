f4b80866087524feccb28e321a9af408e91d0123
YashaswiniMM27 <yashaswinimm2703@gmail.com>
2025-01-20 14:32:32 +0530

    moved the assets to public folder

--- src/components/GameStatus.jsx
+++ src/components/GameStatus.jsx
@@ -2,11 +2,11 @@ export default function GameStatus({isGameOver, isGameWinner}){
     return (
         <section className="statusContainer">
             {isGameWinner && <div className="gameStatusWin">
-            <h3>YOU WIN!<img src="/src/assets/party-popper.png" alt="partPopper" /></h3>
+            <h3>YOU WIN!<img src="/public/assets/win.png" alt="win" /></h3>
             <p>You got the W, no cap!</p>
         </div>}
         {isGameOver && <div className="gameStatusLoose">
-            <h3>YOU LOOSE!<img src="/src/assets/loose.webp" alt="partPopper" /></h3>
+            <h3>YOU LOOSE!<img src="/public/assets/loose.webp" alt="loose" /></h3>
             <p>Well, that was an epic fail</p>
         </div>}
         </section>
