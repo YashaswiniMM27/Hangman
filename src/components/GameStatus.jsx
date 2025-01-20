71142ba57a5c3eac336cf227145cf27c661bc6b4
YashaswiniMM27 <yashaswinimm2703@gmail.com>
2025-01-20 14:46:47 +0530

    fixing url swap

--- src/components/GameStatus.jsx
+++ src/components/GameStatus.jsx
@@ -2,11 +2,11 @@ export default function GameStatus({isGameOver, isGameWinner}){
     return (
         <section className="statusContainer">
             {isGameWinner && <div className="gameStatusWin">
-            <h3>YOU WIN!<img src="/assets/loose.webp" alt="win" /></h3>
+            <h3>YOU WIN!<img src="/assets/win.png" alt="win" /></h3>
             <p>You got the W, no cap!</p>
         </div>}
         {isGameOver && <div className="gameStatusLoose">
-            <h3>YOU LOOSE!<img src="/assets/win.png" alt="loose" /></h3>
+            <h3>YOU LOOSE!<img src="/assets/loose.webp" alt="loose" /></h3>
             <p>Well, that was an epic fail</p>
         </div>}
         </section>
